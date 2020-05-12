d3fed6ffa3018a54ca646be3985c23c335425a43
EC2 Default User <ec2-user@ip-172-31-33-172.us-west-2.compute.internal>
2020-05-12 20:58:14 +0000

    Working ECS Fargate Sample App

--- lib/app-stack.ts
+++ lib/app-stack.ts
@@ -1,9 +1,21 @@
 import * as cdk from '@aws-cdk/core';
+import * as ec2 from '@aws-cdk/aws-ec2';
+import * as ecs from '@aws-cdk/aws-ecs';
+import * as ecs_patterns from '@aws-cdk/aws-ecs-patterns';
 
 export class AppStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, 'MyAppVpc', {maxAzs:2});
+    const cluster = new ecs.Cluster(this, 'MyAppEcsCluster', {vpc:vpc});
+    const fargateService = new ecs_patterns.ApplicationLoadBalancedFargateService(this, 'MyAppFargateService', {
+      cluster: cluster,
+      serviceName: 'MyService',
+      taskImageOptions: { image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample") },
+      listenerPort:80,
+      desiredCount:2,
+      publicLoadBalancer:true
+    });
   }
 }
