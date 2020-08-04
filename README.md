6e2ef8260d0c42b38941d7b71da54836b6c90c5b
EC2 Default User <ec2-user@ip-172-31-33-172.us-west-2.compute.internal>
2020-08-04 22:38:24 +0000

    Updates for 20200804 demo.

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ This example CDK app stack deploys an ALB-fronted ECS Fargate app using the amaz
  * `npm install --save @aws-cdk/aws-ec2 @aws-cdk/aws-ecs @aws-cdk/aws-ecs-patterns`    install+save additional dependencies
 
 ## CDK Usage Commands
- * `cdk diff`    show the differences between your current state and deployed stack
+ * `cdk diff`    show the differences between your current code state and deployed stack
  * `cdk synth > file.yaml`    synthesize the resulting CloudFormation template to a file
  * `cdk deploy`    deploy your stack
  * `cdk destroy`    destroy your stack
--- lib/app-stack.ts
+++ lib/app-stack.ts
@@ -7,15 +7,19 @@ export class AppStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const vpc = new ec2.Vpc(this, 'MyAppVpc', {maxAzs:2});
-    const cluster = new ecs.Cluster(this, 'MyAppEcsCluster', {vpc:vpc});
-    const fargateService = new ecs_patterns.ApplicationLoadBalancedFargateService(this, 'MyAppFargateService', {
-      cluster: cluster,
-      serviceName: 'MyService',
-      taskImageOptions: { image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample") },
-      listenerPort:80,
-      desiredCount:2,
-      publicLoadBalancer:true
-    });
+    const appName = this.node.tryGetContext('app_name');
+    const appImg  = this.node.tryGetContext('app_img');
+    if(appName && appImg) {
+      const vpc = new ec2.Vpc(this, appName + '-vpc', {maxAzs:3});
+      const cluster = new ecs.Cluster(this, appName + '-cluster', {vpc:vpc});
+      const fargateService = new ecs_patterns.ApplicationLoadBalancedFargateService(this, appName + '-service', {
+        cluster: cluster,
+        serviceName: 'MyService',
+        taskImageOptions: { image: ecs.ContainerImage.fromRegistry(appImg) },
+        listenerPort:80,
+        desiredCount:3,
+        publicLoadBalancer:true
+      });
+    }
   }
 }
